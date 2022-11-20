6a542042372cb9caaeba9cd61dcf17caa0c18bd6
DarkYagami <sassaniden@gmail.com>
2022-11-20 22:48:57 +0100

    OptionsInput style

--- src/components/OptionsInput.js
+++ src/components/OptionsInput.js
@@ -3,15 +3,19 @@
 
 function OptionsInput(props) {
   return (
-    <div className = "inline-block">
-      <span className="mr-2">{props.text}:</span>
-      <input
-        className="border-2 border-sky-900 rounded-md resize-none p-1 max-w-[100px] mr-4"
-        type="text"></input>
+    <div className = "flex flex-col">
+      <div className="">
+        <div className="border-0 mr-2 inline-block">{props.text}:</div>
+        <div className="border-0 align-baseline mt-1 inline-block
+        "><input className="" type="textarea"></input></div>
+      </div>
+      <div className="text-sm text-gray-700">
+        {props.descr}
+      </div>
     </div>
   )
-
 }
+
 
 
 export default OptionsInput
