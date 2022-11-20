88ec1787dcb181fd4ffb571b885e9e79ca04cd63
DarkYagami <sassaniden@gmail.com>
2022-11-20 16:42:18 +0100

    setup basic CSS app layout

--- src/App.js
+++ src/App.js
@@ -4,53 +4,32 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+  /* The right-hand column is optional and only displayed on big screens
+  It should only be used for optional things like examples, not for anything
+  app-critical.
+  */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="h-[100vh]">
+      <div className="flex flex-col h-[100%]">
+
+        <div className="bg-sky-400 text-gray-50 font-bold p-4 text-left">
+          QUICK TEXT FIXER
+        </div>
+
+        <div className="flex justify-between grow">
+
+          <div className="bg-red-500 grow">
+            main
+          </div>
+          
+          <div className="bg-yellow-400 hidden lg:block max-w-[33%] ">
+            optional column - only on big screens etc. etc. etc. etc. etc. etc. etc. etc. etc. etc. etc. etc.
+          </div>
+            
+        </div>
+
+      </div>
+    
     </div>
   );
 }
